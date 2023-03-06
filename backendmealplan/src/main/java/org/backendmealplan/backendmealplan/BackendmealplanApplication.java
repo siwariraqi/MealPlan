97334834bf62c90bf6d9a3563eb80fd16b575cbb
siwariraqi <siwari@post.bgu.ac.il>
2023-03-06 18:51:17 +0200

    backend side

---
+++ backendmealplan/src/main/java/org/backendmealplan/backendmealplan/BackendmealplanApplication.java
@@ -0,0 +1,13 @@
+package org.backendmealplan.backendmealplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendmealplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendmealplanApplication.class, args);
+	}
+
+}
---
+++ backendmealplan/src/test/java/org/backendmealplan/backendmealplan/BackendmealplanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backendmealplan.backendmealplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendmealplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
