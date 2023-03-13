d07ff79d9a72151e9542ce4fad4ff50227510343
younis3 <younis3dev@gmail.com>
2023-03-13 05:33:06 +0200

    onboarding screens front

---
+++ frontendmealplan/src/app/mealplan/components/questions/onboarding14/onboarding14.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-onboarding14",
+  template: `
+    <div class="container">
+      <p>onboarding14 works!</p>
+    </div>
+  `,
+  styleUrls: ["./onboarding14.component.scss"],
+})
+export class Onboarding14Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ frontendmealplan/src/app/mealplan/components/questions/onboarding7/onboarding7.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-onboarding7",
+  template: `
+    <div class="container">
+      <p>onboarding7 works!</p>
+    </div>
+  `,
+  styleUrls: ["./onboarding7.component.scss"],
+})
+export class Onboarding7Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
