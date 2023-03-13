19e205acc99beb57ba96e9622d8806d921e29da3
Maha Younis <42297470+maha246@users.noreply.github.com>
2023-03-13 20:41:13 +0200

    deleted not needed imports

--- backendmealplan/src/main/java/org/backendmealplan/backendmealplan/BackendmealplanApplication.java
+++ backendmealplan/src/main/java/org/backendmealplan/backendmealplan/BackendmealplanApplication.java
@@ -3,9 +3,6 @@ package org.backendmealplan.backendmealplan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @SpringBootApplication
 public class BackendmealplanApplication{
 	public static void main(String[] args) {
