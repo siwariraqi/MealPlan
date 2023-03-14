7d7689ca028c5317cf4c401645173abdbbc510b9
siwariraqi <siwari@post.bgu.ac.il>
2023-03-14 21:50:03 +0200

    small changes

--- backendmealplan/src/main/java/org/backendmealplan/backendmealplan/dao/DayPlanIdDAO.java
+++ backendmealplan/src/main/java/org/backendmealplan/backendmealplan/dao/DayPlanIdDAO.java
@@ -2,6 +2,9 @@ package org.backendmealplan.backendmealplan.dao;
 import org.backendmealplan.backendmealplan.beans.DayPlanId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DayPlanIdDAO extends JpaRepository<DayPlanId, Long> {
 
+    List<DayPlanId> findByDayPlanId(Long id);
 }
