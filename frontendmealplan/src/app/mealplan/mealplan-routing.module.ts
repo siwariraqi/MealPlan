271c525394d34bf6e0dc1c46e50188d0d4f99884
muhammedjaradat <48496617+muhammedjaradat@users.noreply.github.com>
2023-03-14 15:57:39 +0200

    add components
    meal, menu-items and menu-items-carousle

--- frontendmealplan/src/app/mealplan/mealplan-routing.module.ts
+++ frontendmealplan/src/app/mealplan/mealplan-routing.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from "@angular/core";
+import { Component, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { MealComponent } from "./pages/dayMeal/meal/meal.component";
+import { MenuItemsCarouselComponent } from "./pages/dayMeal/menu-items-carousel/menu-items-carousel.component";
+import { MenuItemsComponent } from "./pages/dayMeal/menu-items/menu-items.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
 
@@ -12,6 +15,18 @@ const routes: Routes = [
     path: "register",
     component: RegisterComponent,
   },
+  {
+    path:"menu-items",
+    component:MenuItemsComponent,
+  },
+  {
+    path:"menu-items-carousel",
+    component:MenuItemsCarouselComponent
+  },
+  {
+    path:"meal",
+    component:MealComponent
+  }
 ];
 
 @NgModule({
--- frontendmealplan/src/app/mealplan/mealplan.module.ts
+++ frontendmealplan/src/app/mealplan/mealplan.module.ts
@@ -5,9 +5,24 @@ import { MealplanRoutingModule } from "./mealplan-routing.module";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { MenuItemsComponent } from './pages/dayMeal/menu-items/menu-items.component';
+import { MenuItemsCarouselComponent } from './pages/dayMeal/menu-items-carousel/menu-items-carousel.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from '../shared/shared.module';
+import { MatChipsModule } from '@angular/material/chips';
+import { MealComponent } from './pages/dayMeal/meal/meal.component';
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, HomeComponent],
-  imports: [CommonModule, MealplanRoutingModule],
+  declarations: [LoginComponent, RegisterComponent, HomeComponent, MenuItemsComponent, MenuItemsCarouselComponent, MealComponent],
+  imports: [CommonModule, MealplanRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    FlexLayoutModule,
+    SharedModule,
+    MatChipsModule
+
+
+  ],
 })
 export class MealplanModule {}
