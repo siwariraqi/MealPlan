9784675ab7e27babd96ea3a45871251f8f8f39a6
younis3 <younis3dev@gmail.com>
2023-03-14 05:36:52 +0200

    login register

--- frontendmealplan/src/app/mealplan/models/User.ts
+++ frontendmealplan/src/app/mealplan/models/User.ts
@@ -1,22 +1,18 @@
-
 import { UserInfo } from "./UserInfo";
 import { UserFeedback } from "./UserFeedback";
 
-
 export class User {
-
-constructor(
-        public userId?:number ,
-        public firstName?:string,
-        public lastName?:string,
-        public password?:string,
-        public phoneNumber?:string,
-        public userName?:string,
-        public userInfo?:UserInfo,
-        public planId?:number,
-        public paymentsIds?:number[],
-        public ingredientsChangesId?:number[],
-        public feedBackIds?:UserFeedback[])
-        {}
-
+  constructor(
+    public userId?: number,
+    public firstName?: string,
+    public lastName?: string,
+    public password?: string,
+    public phoneNumber?: string,
+    public email?: string,
+    public userInfo?: UserInfo,
+    public planId?: number,
+    public paymentsIds?: number[],
+    public ingredientsChangesId?: number[],
+    public feedBackIds?: UserFeedback[]
+  ) {}
 }
