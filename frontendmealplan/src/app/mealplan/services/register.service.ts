1aa4a3d571c0c37c36f9559945e371a6b00e83c4
younis3 <younis3dev@gmail.com>
2023-03-16 22:47:19 +0200

    register

--- frontendmealplan/src/app/mealplan/services/register.service.ts
+++ frontendmealplan/src/app/mealplan/services/register.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "express";
+import { Observable } from "rxjs";
+import { User } from "../models/User";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RegisterService {
+  // private BASE_URL: string = "http://localhost:8080/users/adduser";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private router: Router) {}
+
+  // registerUser(user: User): Observable<object> {
+  //   return this.httpClient.post(this.BASE_URL, user);
+  // }
 }
