895b7a6e0eff4f1b483d990c6aca5058e0cc7ce0
Amir Mahamid <amirmahamed7@gmail.com>
2023-03-19 00:07:06 +0200

    use api service

--- frontendmealplan/src/app/mealplan/services/user.service.ts
+++ frontendmealplan/src/app/mealplan/services/user.service.ts
@@ -1,23 +1,24 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, observable, Observable, throwError } from 'rxjs';
 import { User } from '../models/User';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  SERVER_BASE_URL = 'http://127.0.0.1:8080/users/';
-  CHOOSEPLAN = this.SERVER_BASE_URL+"choosePlan";
-  UPDATEPROFILE = this.SERVER_BASE_URL+"updateProfile";
-  GETUSER = this.SERVER_BASE_URL+"getUser";
 
-  constructor(private http:HttpClient) { }
+  CHOOSEPLAN = 'users/choosePlan?userId=';
+  UPDATEPROFILE = 'users/updateProfile';
+  GETUSER = 'users/getUser?userId=';
+
+  constructor(private http:HttpClient, private apiService:ApiService) { }
 
   getUser(userId: number): Observable<User> {
-    const params = new HttpParams().set('userId', String(userId));
-    return this.http.get<User>(this.GETUSER, { params })
+    const params = new HttpHeaders().set('userId', String(userId));
+    return this.apiService.get<User>(this.GETUSER+ `${userId}`)
       .pipe(
         catchError(error => {
           if (error.status === 404) {
@@ -28,18 +29,25 @@ export class UserService {
       );
   }
 
+  
   updateProfile(user:User):Observable<string>{
-    return this.http.post<any>(this.UPDATEPROFILE,user);
+    return this.apiService.post<any>(this.UPDATEPROFILE,user);
   }
 
   choosePlan(userId: number, planId: number): Observable<string> {
-
-    let params = new HttpParams()
+    const params = new HttpParams()
       .set('userId', userId.toString())
       .set('planId', planId.toString());
 
-
-    return this.http.post<string>(this.CHOOSEPLAN, null, { params: params });
+    return this.apiService.post<string>(this.CHOOSEPLAN+`${userId}` +"&planId="+`${planId}`  )
+      .pipe(
+        catchError(error => {
+          if (error.status === 404) {
+            return throwError('User or plan not found');
+          }
+          return throwError('Something went wrong');
+        })
+      );
   }
 
 }
