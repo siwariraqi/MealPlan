a1216dcb03818bc04d2bfbee9c8da81b206f29d7
muhammedjaradat <48496617+muhammedjaradat@users.noreply.github.com>
2023-03-23 02:09:05 +0200

    add Diet Type model and conditions to show diet types

--- frontendmealplan/src/app/mealplan/pages/dayMeal/diet-type/diet-type.component.ts
+++ frontendmealplan/src/app/mealplan/pages/dayMeal/diet-type/diet-type.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { DietType } from 'src/app/mealplan/models/DietType';
+import { DayMealService } from 'src/app/mealplan/services/day-meal.service';
+
 
 @Component({
   selector: 'app-diet-type',
@@ -6,14 +9,39 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./diet-type.component.scss']
 })
 export class DietTypeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
   keto=true;
   vegan=true;
   gluten=true;
   dairy=true;
+  mealDietType:DietType[];
+  constructor(private dayMealService:DayMealService) { }
+
+  ngOnInit(): void {
+    this.getMealDietType();
+    this.mealDietTypeConditions();
+  }
+
+
+  public getMealDietType () {
+    this.mealDietType=this.dayMealService.getMealDietType();
+  }
+
+  public mealDietTypeConditions(){
+    for(let i=0;i<this.mealDietType.length;i++)
+    {
+    if(this.mealDietType[i].text=="keto"){
+      this.keto=true;
+    }
+    if(this.mealDietType[i].text=="vegan"){
+      this.vegan=true;
+    }
+    if(this.mealDietType[i].text=="gluten"){
+      this.gluten=true;
+    }
+    if(this.mealDietType[i].text=="dairy"){
+      this.dairy=true;
+    }
+  }
+  }
 
 }
