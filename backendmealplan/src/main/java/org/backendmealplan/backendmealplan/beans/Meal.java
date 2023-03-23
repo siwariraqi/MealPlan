708cf11853be65c72c56bf50eddf2f48f1de01c7
siwariraqi <siwari@post.bgu.ac.il>
2023-03-23 02:55:13 +0200

    diet type

--- backendmealplan/src/main/java/org/backendmealplan/backendmealplan/beans/Meal.java
+++ backendmealplan/src/main/java/org/backendmealplan/backendmealplan/beans/Meal.java
@@ -1,11 +1,14 @@
 package org.backendmealplan.backendmealplan.beans;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -50,12 +53,13 @@ public class Meal {
     @NotBlank
     private String tips;
 
-//    @ManyToMany
-//    @JoinTable(
-//            name = "meal_diet_types",
-//            joinColumns = @JoinColumn(name = "meal_id"),
-//            inverseJoinColumns = @JoinColumn(name = "diet_type_id"))
-//    Set<DietTypes> dietTypes;
+    @ManyToMany
+    @Fetch(FetchMode.JOIN) // Eagerly load the collection
+    @JoinTable(
+            name = "meal_diet_types",
+            joinColumns = @JoinColumn(name = "meal_id"),
+            inverseJoinColumns = @JoinColumn(name = "diet_type_id"))
+    Set<DietType> dietTypes;
 
     @ToString.Exclude
     @JsonIgnore
