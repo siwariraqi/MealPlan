97deae9ebc158b02eceb65034b5e6bbd7627935d
muhammedjaradat <48496617+muhammedjaradat@users.noreply.github.com>
2023-03-26 02:47:08 +0300

    add meal UI in Admin
    This reverts commit 156bd7c2d90071e347f2febe62c1d5d43ef9b3a2.

--- frontendmealplan/src/app/admin-meal-plan/menu-items/menu-items.module.ts
+++ frontendmealplan/src/app/admin-meal-plan/menu-items/menu-items.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'; 
 import { SharedModule } from '../../shared/shared.module';
-import { CategoriesComponent } from './categories/categories.component'; 
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { AddComponent } from './add/add.component';
-import { CategoryDialogComponent } from './categories/category-dialog/category-dialog.component';
+
 
 export const routes: Routes = [ 
   { path: '', redirectTo: 'list', pathMatch: 'full'},
-  { path: 'categories', component: CategoriesComponent, data: { breadcrumb: 'Categories' } },
   { path: 'list', component: ListComponent, data: { breadcrumb: 'Menu Items' } },
   { path: 'detail', component: DetailComponent, data: { breadcrumb: 'Menu Item Detail' } },
   { path: 'detail/:id', component: DetailComponent, data: { breadcrumb: 'Menu Item Detail' } }, 
@@ -20,10 +18,9 @@ export const routes: Routes = [
 
 @NgModule({
   declarations: [
-    CategoriesComponent, 
     ListComponent, 
     DetailComponent, 
-    AddComponent, CategoryDialogComponent
+    AddComponent,
   ],
   imports: [
     CommonModule,
