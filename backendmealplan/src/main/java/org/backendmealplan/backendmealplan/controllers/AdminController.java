993223338871aa14ab2f7d452416649a36ec2a66
siwariraqi <siwari@post.bgu.ac.il>
2023-03-26 20:26:04 +0300

    fix the admin authentication

--- backendmealplan/src/main/java/org/backendmealplan/backendmealplan/controllers/AdminController.java
+++ backendmealplan/src/main/java/org/backendmealplan/backendmealplan/controllers/AdminController.java
@@ -4,9 +4,13 @@ import org.backendmealplan.backendmealplan.bl.UserBL;
 import org.backendmealplan.backendmealplan.exceptions.PlanNotExistedException;
 import org.backendmealplan.backendmealplan.exceptions.UNAUTHORIZEDException;
 import org.backendmealplan.backendmealplan.exceptions.userNotFoundException;
+import org.backendmealplan.backendmealplan.security.AuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,7 +37,7 @@ public class AdminController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity deleteUser(@RequestParam Long adminId, @RequestParam Long userId) {
+    public ResponseEntity deleteUser(@RequestParam Long userId) {
         try {
             userBL.deleteUser(userId);
             return ResponseEntity.ok().build();
@@ -42,8 +46,8 @@ public class AdminController {
         }
     }
 
-    @PutMapping("/resetUser")
-    public ResponseEntity resetUser(@RequestParam Long adminId ,@RequestParam Long userId) {
+    @PutMapping("/reset")
+    public ResponseEntity resetUser(@RequestParam Long userId) {
         try {
             userBL.resetUser(userId);
             return ResponseEntity.ok().build();
@@ -53,7 +57,7 @@ public class AdminController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity updateUserPlan(@RequestParam Long adminId, @RequestParam Long userId, @RequestParam String planName) {
+    public ResponseEntity updateUserPlan(@RequestParam Long userId, @RequestParam String planName) {
         try {
             User user = userBL.updateUserPlan(userId, planName);
             return (ResponseEntity) ResponseEntity.ok(user);
@@ -65,7 +69,7 @@ public class AdminController {
     }
 
     @PutMapping("/changeRole")
-    public ResponseEntity UpdateUserRole(@RequestParam Long adminId, @RequestParam Long userId, @RequestParam Boolean isAdmin) {
+    public ResponseEntity UpdateUserRole(@RequestParam Long userId, @RequestParam Boolean isAdmin) {
         try {
             userBL.updateUserRole(userId, isAdmin);
             return ResponseEntity.ok().build();
@@ -73,4 +77,16 @@ public class AdminController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
+
+//    @Autowired
+//    private AuthenticationFilter authenticationFilter;
+//
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.addFilterBefore(authenticationFilter, BasicAuthenticationFilter.class)
+//                .authorizeRequests()
+//                .antMatchers("/admin/**").hasRole("Admin")
+//                .anyRequest().permitAll()
+//                .and().httpBasic()
+//                .and().csrf().disable();
+//    }
 }
