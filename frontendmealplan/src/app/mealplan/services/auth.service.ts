a15170c9ff9a54b002068a4bb738c42b708453d1
Younis3 <91804311+younis3@users.noreply.github.com>
2023-03-26 18:07:34 +0300

    Update auth.service.ts

--- frontendmealplan/src/app/mealplan/services/auth.service.ts
+++ frontendmealplan/src/app/mealplan/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 import { User } from "../models/User";
 
