e4c3623a4145bcc21dfb5d68533cec024b0edc82
siwariraqi <siwari@post.bgu.ac.il>
2023-03-28 06:24:31 +0300

    minor

--- backendmealplan/src/main/java/org/backendmealplan/backendmealplan/controllers/UsersController.java
+++ backendmealplan/src/main/java/org/backendmealplan/backendmealplan/controllers/UsersController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
